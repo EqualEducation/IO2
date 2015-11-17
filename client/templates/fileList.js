7f4daf4fed535b42438c5f111c75bd57444c0932
Benji Meltzer <benjimeltzer@gmail.com>
2015-11-17 23:49:58 +0200

    added file detail collection and linked with collectionFS. Also implemented version 0 search

--- client/templates/fileList.js
+++ client/templates/fileList.js
@@ -1,33 +1,42 @@
   Meteor.subscribe("fileUploads");
+  Meteor.subscribe("fileMeta");
   Template.fileList.helpers({
     theFiles: function () {
       var docName=Session.get('docName');
-      console.log(docName);
+      //console.log(YourFileCollection.findOne());
       //console.log(docName);
       //console.log(YourFileCollection.find({name:'Activist Wordsearch.docx'}).original.name);
       //console.log(YourFileCollection.find({name: 'Activism 101.docx'}).fetch())
+     // console.log(fileDetails.findOne());
       return YourFileCollection.find({"original.name":new RegExp(docName)});
     }
   });
     Template.fileList.events({
     'click #deleteFileButton ': function (event) {
-      console.log("deleteFile button ", this);
+      var fsId= this._id;
+      var fileDetailsId=fileDetails.findOne({fileId:fsId})._id;
       YourFileCollection.remove({_id: this._id});
+      fileDetails.remove({_id:fileDetailsId});
     },
     'change .your-upload-class': function (event, template) {
       console.log("uploading...")
       FS.Utility.eachFile(event, function (file) {
         console.log("each file...");
         var yourFile = new FS.File(file);
+        console.log('yourfile')
+        console.log(yourFile)
         YourFileCollection.insert(yourFile, function (err, fileObj) {
           console.log("callback for the insert, err: ", err);
           if (!err) {
-            console.log("inserted without error");
+            console.log("inserted without error",fileObj)
+            fileDetails.insert({name:yourFile.original.name,fileId:fileObj._id});
+            console.log(fileDetails.find());
           }
           else {
             console.log("there was an error", err);
           }
         });
+        //
       });
     },
      'submit form': function(event,template){
--- server/publish.js
+++ server/publish.js
@@ -2,3 +2,7 @@ Meteor.publish("fileUploads", function () {
     console.log("publishing fileUploads");
     return YourFileCollection.find();
   });
+Meteor.publish("fileMeta", function () {
+    console.log("publishing fileMeta");
+   return fileDetails.find();
+  });
