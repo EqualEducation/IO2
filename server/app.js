eb492d5b30ac52df9e1301d6593f2ecdb2de5619
Benji Meltzer <benjimeltzer@gmail.com>
2015-12-14 18:36:28 +0200

    allowed for searching specific attributes e.g. name, description, keywords

--- server/app.js
+++ server/app.js
@@ -3,19 +3,48 @@ SearchSource.defineSource('filesToSearch', function(searchText, options) {
 
   if(searchText) {
     var regExp = buildRegExp(searchText);
-    var selector = {$or: [
+    var containsColon=regExp.toString().indexOf(":");
+    var containsName=regExp.toString().indexOf("name");
+    var containsDescription=regExp.toString().indexOf("description");
+    var containsKeywords=regExp.toString().indexOf("keyword");
+    var selector=null;
+    if(containsColon<0)
+    {
+    selector = {$or: [
       {name: regExp},
       {description: regExp},
       {keywords: regExp}
     ]};
+  }
+  //if they have entered a colon
+  else
+  {
+    var stringLength=regExp.toString().length;
+    var searchKey=regExp.toString().substring(containsColon+1,stringLength-4);
+    var regExpSearchKey=buildRegExp(searchKey);
+    if(containsName>0)
+    {
+    selector={name: regExpSearchKey};
+    }
+    if(containsDescription>0)
+    {
+    selector={description: regExpSearchKey};
+    }
+    if(containsKeywords>0)
+    {
+    selector={keywords: regExpSearchKey};
+    }
+  }
     return fileDetails.find(selector, options).fetch();
-  } else {
+  }
+//if nothing in search - return everything
+  else {
     return fileDetails.find({}, options).fetch();
   }
 });
 
 function buildRegExp(searchText) {
   // this is a dumb implementation
-  var parts = searchText.trim().split(/[ \-\:]+/);
+  var parts = searchText.trim().split(/[ \-]+/);
   return new RegExp("(" + parts.join('|') + ")", "ig");
 }
