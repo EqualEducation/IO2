077ad26b3f25c7ba81a7f00db97b576b3a12fa0d
Carla Goldstein <carla.goldstein@gmail.com>
2016-01-18 22:54:38 +0200

    added multiple modals for wizard

--- client/templates/upload/uploadResource.js
+++ client/templates/upload/uploadResource.js
@@ -1,10 +1,26 @@
 Template.uploadResource.events({
 	'click #uploadResourceButton': function(event, template) {
-		$('.ui.upload.modal')
-			.modal({
-	    	onApprove : function() {
-	    	}
-	  	})
- 			.modal('show');
+		$('.coupled.modal')
+		  .modal({
+		    allowMultiple: false
+		  })
+		;
+		// attach events to buttons
+		$('.uploadDetails.modal')
+		  .modal('attach events', '.chooseType.modal #next')
+		;
+		// show first now
+		$('.chooseType.modal')
+			.modal('show')
+		;
+
+		$('.chooseType.modal')
+			.modal('attach events', '.uploadDetails.modal #back')
+		;
+	},
+	'click #next': function(event, template) {
+		var element = template.find('input:radio[name=example2]:checked');
+		console.log(template.find('input:radio[name=example2]'));
+
 	}
 })
