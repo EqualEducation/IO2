68a7effb4f19fbaa66e82e09bdacde4b803606ad
Benji Meltzer <benjimeltzer@gmail.com>
2016-01-25 12:15:47 +0200

    download button working

--- client/templates/activityDetail.js
+++ client/templates/activityDetail.js
@@ -1,8 +1,18 @@
 Template.activityDetail.helpers({
   activity: function() {
     var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
-    //var details = YourFileCollection.findOne(file.fileId);
-    return file
+    var details = YourFileCollection.findOne(file.fileId);
+    console.log(details);
+    return {
+      URL: details.url,
+      name: file.name,
+      type: details.original.type,
+      isUploaded: details.original.updatedAt,
+      details:details,
+      keywords: file.keywords,
+      description:file.description,
+      _id:details._id
+    };
   },
   uniqueKeywords: function(){
 //return all unique keywords (ignores blanks)
