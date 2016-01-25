ec29df419e9c9967259c53a2dd9c81118cc800d8
Carla Goldstein <carla.goldstein@gmail.com>
2016-01-25 16:49:09 +0200

    added dropdown initialization on edit screen

--- client/templates/activityDetail.js
+++ client/templates/activityDetail.js
@@ -1,3 +1,10 @@
+
+Template.activityDetail.onRendered(function () {
+  $('.ui.search.dropdown')
+    .dropdown()
+  ;
+});
+
 Template.activityDetail.helpers({
   activity: function() {
     var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
@@ -25,5 +32,3 @@ return _.uniq(_.flatten(fileDetails.find({keywords:{$not:""}}, {
 
   }
 })
-
-
