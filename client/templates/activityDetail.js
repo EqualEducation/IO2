f7f9f3d3c90e77dcb952d5296d5ca74b61aee313
Benji Meltzer <benjimeltzer@gmail.com>
2016-01-28 12:52:44 +0200

    updated activities

--- client/templates/activityDetail.js
+++ client/templates/activityDetail.js
@@ -1,3 +1,33 @@
+Template.activityDetail.onRendered(function () {
+  $("#deleteFile").click(function(){
+    //var fsId= Session.get('fileDetailsID');
+      var fileDetailsId=Session.get('fileDetailsID');
+      var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
+      YourFileCollection.remove({_id: file.fileId});
+      fileDetails.remove({_id:fileDetailsId});
+      Session.set('fileSearch',fileSearch.getData());
+      fileSearch.cleanHistory();
+      fileSearch.search("");
+      //MAKE THE MODAL CLOSE!
+      $('.ui.activitydetail.modal').modal("hide");
+  })
+  // console.log('here');
+  // $('.ui.fluid.search.dropdown')
+  //   .dropdown()
+  // ;
+});
+// Template.activityDetail.events({
+//     'click #deleteFile': function (event) {
+//       console.log('here');
+//       // var fsId= this._id;
+//       // var fileDetailsId=fileDetails.findOne({fileId:fsId})._id;
+//       // YourFileCollection.remove({_id: this._id});
+//       // fileDetails.remove({_id:fileDetailsId});
+//       // Session.set('fileSearch',fileSearch.getData());
+//       // fileSearch.cleanHistory();
+//       // fileSearch.search("");
+//     }
+//   });
 Template.activityDetail.helpers({
   activity: function() {
     var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
@@ -20,8 +50,6 @@ return _.uniq(_.flatten(fileDetails.find({keywords:{$not:""}}, {
 }).fetch().map(function(x) {
     return x.keywords;
 }), true)).sort();
-
-
   }
 })
 
