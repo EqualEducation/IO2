e6d7d189f319990bcf07a34e6d38bd30ac21c1bb
Benji Meltzer <benjimeltzer@gmail.com>
2016-01-30 13:28:58 +0200

    removed error messages at start of session

--- client/templates/activityDetail.js
+++ client/templates/activityDetail.js
@@ -15,18 +15,23 @@ Template.activityDetail.onRendered(function () {
 
 Template.activityDetail.helpers({
   activity: function() {
-    var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
-    var details = YourFileCollection.findOne(file.fileId);
-    return {
-      URL: details.url,
-      name: file.name,
-      type: details.original.type,
-      isUploaded: details.original.updatedAt,
-      details:details,
-      keywords: file.keywords,
-      description:file.description,
-      _id:details._id
-    };
+    if(Session.get('fileDetailsID')===undefined){
+      return 0;
+    }
+    else{
+      var file=fileDetails.findOne({_id:Session.get('fileDetailsID')});
+      var details = YourFileCollection.findOne(file.fileId);
+      return {
+        URL: details.url,
+        name: file.name,
+        type: details.original.type,
+        isUploaded: details.original.updatedAt,
+        details:details,
+        keywords: file.keywords,
+        description:file.description,
+        _id:details._id
+      };
+  }
   },
   uniqueKeywords: function(){
 //return all unique keywords (ignores blanks)
