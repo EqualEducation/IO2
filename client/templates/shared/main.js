7a287895a22953721ef20e69eec1a51984a2ddc7
Carla Goldstein <carla.goldstein@gmail.com>
2016-02-16 15:10:36 +0200

    added custom primary color and nav bar

---
+++ client/templates/shared/main.js
@@ -0,0 +1,13 @@
+// Template.footer.onRendered(function() {
+//   // .ui.fixed.sticky + p {
+//   //   margin-top: 39px;
+//   // }
+//
+//   $('.ui.sticky')
+//     .sticky({
+//       // offset       : 0,
+//       bottomOffset : 0,
+//       context      : '#main'
+//     })
+//   ;
+// });
