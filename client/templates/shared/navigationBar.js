b17e2a14e5c80d61712b29987a052d2cc3b7ea2f
Carla Goldstein <carla.goldstein@gmail.com>
2016-02-23 21:25:23 +0200

    added create buttons

--- client/templates/shared/navigationBar.js
+++ client/templates/shared/navigationBar.js
@@ -1,5 +1,6 @@
 Template.navigationBar.events({
   'click .item': function(event,template){
+    console.log('navigation bar');
     var currentTabStatus=$( event.target);
     currentTabStatus.addClass( "active" );
     $( "a" ).not( currentTabStatus ).removeClass( "active" );
