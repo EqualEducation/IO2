855f223e140284dd7709ceb26994bfa3b7c16062
Benji Meltzer <benjimeltzer@gmail.com>
2016-03-22 17:46:07 +0200

    file download structure implemented

--- client/templates/search/view/viewItemContent.js
+++ client/templates/search/view/viewItemContent.js
@@ -1,3 +1,16 @@
+Template.viewItemContent.helpers({
+  file: function(itemID) {
+    console.log(itemID);
+    var linkedFile=(YourFileCollection.find(itemID).fetch());
+    //linkedFile=YourFileCollection.findOne();
+    console.log("FILE")
+    console.log(linkedFile)
+    return linkedFile;
+    //return itemType.toLowerCase() + ".view";
+  }
+})
+
+
 Template.viewItemContent.events({
     'click #deleteFile': function (event) {
       //console.log(this);
@@ -16,59 +29,10 @@ Template.viewItemContent.events({
         })
         .modal('show')
       ;
-
-
-
-
-      // fileSearch.cleanHistory();
-      // fileSearch.search("");
-//       console.log('sessionID');
-//       console.log(Session.get('resourceDetailsID'));
-//       var item=this;
-//       var itemKeywords=item.keywords;
-//       var itemDescription=item.description;
-//       $form=$('.ui.form');
-//       var keywords=itemKeywords;//.join()
-//       //Session.set('resourceDetailsID',item._id);
-//       $('.ui.editItem.modal')
-//         .modal({
-//           onDeny    : function(){
-//           return true;
-//           },
-//           onApprove : function() {
-//             var keywordsVar=$form.form('get value', 'keywords');
-//             var descriptionVar=$form.form('get value', 'description');
-//             var array=keywordsVar.split(',');
-//             var resourceDetailsID=Session.get('resourceDetailsID');
-//             Resources.update(item._id,{$set: {"keywords":array,"description":descriptionVar}});
-//             return true;
-//         }
-//         })
-//       .modal('show')
-//       ;
-//       $('.ui.form')
-//   .form({
-//     fields: {
-//       name: {
-//         identifier: 'keywords',
-//         rules: [
-//           {
-//             type   : 'empty',
-//             prompt : 'Please enter keywords'
-//           }
-//         ]
-//       },
-//       skills: {
-//         identifier: 'description',
-//         rules: [
-//           {
-//             type   : 'empty',
-//             prompt : 'Please enter description'
-//           }
-//         ]
-//       }
-//     }
-//   })
-// ;
     }
+    // },
+    // 'click #downloadFile': function (event) {
+    //   console.log("DOWNLOAD");
+    //   console.log(this);
+    // }
     });
