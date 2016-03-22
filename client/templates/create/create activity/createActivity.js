706a66ad80d182d39f57c71e27a508e44d59a066
Carla Goldstein <carla.goldstein@gmail.com>
2016-03-22 18:41:46 +0200

    removed modals for activity and curriculum. work in progress

--- client/templates/create/create activity/createActivity.js
+++ client/templates/create/create activity/createActivity.js
@@ -1,26 +1,26 @@
-Template.createActivity.onRendered(function() {
-  $('.createActivity.modal')
-      .modal({
-        onApprove : function() {
-               var form = $('#activityDetailsForm');
-               var topic =	form.form('get field', 'topic').val();
-               var allFields = form.form('get values')
-               console.log('SAVING');
-               console.log(allFields)
-
-               var newActivity = new Object();
-
-               console.log(allFields.keywords);
-               newActivity.details = allFields;
-               newActivity.fileIDs = Session.get('fileIDs');
-               Meteor.call("addItem", ItemTypeEnum.ACTIVITY, newActivity);
-               Session.set('fileIDs', null);
-               form.form('clear')
-              return true;
-            }
-           })
-         ;
-})
+// Template.cre.onRendered(function() {
+//   $('.createActivity.modal')
+//       .modal({
+//         onApprove : function() {
+//                var form = $('#activityDetailsForm');
+//                var topic =	form.form('get field', 'topic').val();
+//                var allFields = form.form('get values')
+//                console.log('SAVING');
+//                console.log(allFields)
+//
+//                var newActivity = new Object();
+//
+//                console.log(allFields.keywords);
+//                newActivity.details = allFields;
+//                newActivity.fileIDs = Session.get('fileIDs');
+//                Meteor.call("addItem", ItemTypeEnum.ACTIVITY, newActivity);
+//                Session.set('fileIDs', null);
+//                form.form('clear')
+//               return true;
+//             }
+//            })
+//          ;
+// })
 
 
 Template.uploadGuide.events({
--- client/templates/create/create resource/createResource.js
+++ client/templates/create/create resource/createResource.js
@@ -1,52 +1,52 @@
-Template.createResource.onRendered(function() {
-  //setting up chooseType modal
-  $('.chooseType.modal')
-    //present chooseType modal when "back" button clicked on resourceDetails modal
-    .modal('attach events', '.resourceDetails.modal #back')
-    //what to do when approve button pressed ("next")
-    .modal({
-       onApprove : function() {
-        var resourceType = $('input:radio[name=example2]:checked').val();
-        var modalName = '.'+resourceType+'.modal';
-        Session.set('activeModal', resourceType);
-       }
-    });
+// Template.create_resource.onRendered(function() {
+//   //setting up chooseType modal
+//   $('.chooseType.modal')
+//     //present chooseType modal when "back" button clicked on resourceDetails modal
+//     .modal('attach events', '.resourceDetails.modal #back')
+//     //what to do when approve button pressed ("next")
+//     .modal({
+//        onApprove : function() {
+//         var resourceType = $('input:radio[name=example2]:checked').val();
+//         var modalName = '.'+resourceType+'.modal';
+//         Session.set('activeModal', resourceType);
+//        }
+//     });
+//
+//   $('.resourceDetails.modal')
+//     //present resourceDetails modal when "next" button clicked on chooseType modal
+//     .modal('attach events', '.chooseType.modal #next')
+//     //what to do when approve button pressed ("save")
+//     .modal({
+//        onApprove : function() {
+//          var form = $('#resourceDetailsForm');
+//           var topic =	form.form('get field', 'topic').val();
+//           var allFields = form.form('get values')
+//           console.log('SAVING');
+//           console.log(allFields);
+//
+//           var newResource = new Object();
+//
+//           newResource.type = Session.get('activeModal')
+//           var subTopic = allFields.subTopic;
+//           var keywords = allFields.keywords;
+//           var methods = allFields.methods;
+//           var materials = allFields.materials;
+//
+//           newResource.details = allFields;
+//           newResource.fileIDs = Session.get('fileIDs');
+//           Session.set('fileIDs', null);
+//           Meteor.call("addItem", ItemTypeEnum.RESOURCE, newResource);
+//           form.form('clear')
+//         //  $('.ui.form').submit();
+//          //Return false as to not close modal dialog
+//          return true;
+//        }
+//       })
+//     ;
+// })
 
-  $('.resourceDetails.modal')
-    //present resourceDetails modal when "next" button clicked on chooseType modal
-    .modal('attach events', '.chooseType.modal #next')
-    //what to do when approve button pressed ("save")
-    .modal({
-       onApprove : function() {
-         var form = $('#resourceDetailsForm');
-          var topic =	form.form('get field', 'topic').val();
-          var allFields = form.form('get values')
-          console.log('SAVING');
-          console.log(allFields);
 
-          var newResource = new Object();
-
-          newResource.type = Session.get('activeModal')
-          var subTopic = allFields.subTopic;
-          var keywords = allFields.keywords;
-          var methods = allFields.methods;
-          var materials = allFields.materials;
-
-          newResource.details = allFields;
-          newResource.fileIDs = Session.get('fileIDs');
-          Session.set('fileIDs', null);
-          Meteor.call("addItem", ItemTypeEnum.RESOURCE, newResource);
-          form.form('clear')
-        //  $('.ui.form').submit();
-         //Return false as to not close modal dialog
-         return true;
-       }
-      })
-    ;
-})
-
-
-Template.createResource.helpers({
+Template.create_resource.helpers({
   content: function() {
 		var ret = 'content_' + Session.get('activeModal')
 		return ret ;
