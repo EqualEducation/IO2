d0279104017c93b64f7dcf1d715bb2ccaf2ce15a
Carla Goldstein <carla.goldstein@gmail.com>
2016-03-23 12:31:47 +0200

    updated dev mongo url to point to heroic

--- config/dev_env.sh
+++ config/dev_env.sh
@@ -1,2 +1,2 @@
-export MONGO_URL="mongodb://ee:ee@sg-mother1-6242.servers.mongodirector.com:27017/ietu_meteor_com"
+export MONGO_URL="mongodb://heroku_h107bkgb:ifrf0fa95qbe6fm02ovn6rdjht@ds013579.mlab.com:13579/heroku_h107bkgb"
 export METEOR_SETTINGS="$(cat config/settings.json)"
