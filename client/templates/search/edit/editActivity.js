9be9f3494fefda83c2a55b4498e5b008b107f7fe
Carla Goldstein <carla.goldstein@gmail.com>
2016-03-24 09:06:12 +0200

    layout and form validation for edit screen

--- client/templates/search/edit/editActivity.js
+++ client/templates/search/edit/editActivity.js
@@ -1,9 +1,42 @@
 Template.editActivity.onRendered( function() {
 	$('#activityDetailsForm')
+  .form('set values', {
+    title     : this.data.details.title,
+    description     : this.data.details.description,
+    mainTopic   : this.data.details.mainTopic,
+    subTopic   : this.data.details.subTopic,
+    keywords : this.data.details.keywords,
+    audience : this.data.details.audience,
+    source    : this.data.details.source,
+    method    : this.data.method,
+    books    : this.data.books,
+    films    : this.data.films
+  })
   .form({
-    onSuccess : function(event){
-
+		onFailure(formErrors, fields)	{
+			event.preventDefault();
+			console.log(formErrors);
+			return false;
+		},
+		onSuccess : function(event, fields){
+			event.preventDefault();
+      console.log('edit activity');
+      console.log(this);
+			// var form = $('#activityDetailsForm');
+			// var topic =	form.form('get field', 'topic').val();
+			// var newActivity = new Object();
+      //
+			// newActivity.details = fields;
+			// newActivity.fileIDs = Session.get('fileIDs');
+			// newActivity.guideID = Session.get('guideID');
+			// Meteor.call("updateItem", ItemTypeEnum.ACTIVITY, newActivity);
+			// Session.set('fileIDs', null);
+			// form.form('clear')
+			// console.log('Success');
+			// Router.go('create');
+			return false;
     },
+		on: 'submit',
     fields: {
       title     : 'empty',
       mainTopic   : 'empty',
@@ -13,17 +46,4 @@ Template.editActivity.onRendered( function() {
       audience    : 'empty'
     }
   })
-	.form('set values', {
-    title     : this.data.details.title,
-		description     : this.data.details.description,
-    mainTopic   : this.data.details.mainTopic,
-    subTopic   : this.data.details.subTopic,
-    keywords : this.data.details.keywords,
-    audience : this.data.details.audience,
-    source    : this.data.details.source,
-		method    : this.data.method,
-		books    : this.data.books,
-		films    : this.data.films
-  })
-;
 })
