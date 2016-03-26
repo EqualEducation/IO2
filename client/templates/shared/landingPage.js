df6721eb955c7efe39716b449cc908c39672bd50
Benji Meltzer <benjimeltzer@gmail.com>
2016-03-26 10:06:33 +0200

    added url decoding for search

--- client/templates/shared/landingPage.js
+++ client/templates/shared/landingPage.js
@@ -1,8 +1,20 @@
 Template.landingPage.events({
     'click #searchButton': function (event) {
       var searchText=$('#search-box').val();
-      var URL=('\\search\\'+searchText);
+      var encodedText=encode(searchText)
+      var URL=('\\search\\'+encodedText);
       window.location = URL;
     }
     });
 
+
+
+function encode(text) {
+  var unencoded = text;
+  return encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");
+}
+function decode() {
+  var obj = document.getElementById('dencoder');
+  var encoded = obj.value;
+  obj.value = decodeURIComponent(encoded.replace(/\+/g,  " "));
+}
