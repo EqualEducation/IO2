a1cc82c321c92a8b4cd006b2665421a2c72be553
Carla Goldstein <carla.goldstein@gmail.com>
2016-03-30 22:12:25 +0200

    added all resources to creating an activity

--- client/templates/create/create activity/connectResources.js
+++ client/templates/create/create activity/connectResources.js
@@ -1,9 +1,19 @@
 Template.registerHelper("allResourcesForType", function (type) {
-  var resources = Resources.find({'type': type});
+  if (type != undefined) {
+    var resources = Resources.find({'type': type});
+    return resources;
+  }
+
+  var resources = Resources.find({});
   return resources;
 });
 
 Template.connectBooks.onRendered(function() {
   $('.connectResources.ui.dropdown')
+    .dropdown();
+})
+
+Template.connectResources.onRendered(function() {
+  $('.ui.fluid.multiple.search')
     .dropdown();
 })
