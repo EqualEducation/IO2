1046ece9e987d31a14a7bec0da8b19ce6324eaf2
Carla Goldstein <carla.goldstein@gmail.com>
2016-04-19 15:23:53 +0200

    put back remove file function

--- client/templates/shared/upload file/uploadFile.js
+++ client/templates/shared/upload file/uploadFile.js
@@ -1,4 +1,18 @@
 Template.uploadFile.events({
+	'click #removeFile': function (event) {
+		console.log('remove file!');
+		$('.ui.basic.change.modal')
+				.modal({
+					onDeny    : function(){
+						return true;
+					},
+					onApprove: function(){
+						Session.set('fileIDs',null);
+						return true;
+					}
+				})
+				.modal('show')
+	},
 		'change .uploadFile': function (event, template) {
 			console.log('upload file');
 			console.log(this.fileType)
@@ -10,13 +24,15 @@ Template.uploadFile.events({
 	},
 })
 
-Template.uploadFile.onRendered(function() {
-
-})
-
 Template.uploadFile.helpers({
-	getFileNames: function(fileIDs){
-    return YourFileCollection.findOne(fileIDs);
+	getFileNames: function(fileType){
+		var fileIDs;
+		if (fileType == 'guide') {
+			fileIDs = Session.get('guideID')
+		} else {
+			fileIDs = Session.get('fileIDs')
+		}
+		return YourFileCollection.findOne(fileIDs);
   },
   noAssociatedFiles: function(fileIDs){
     if (fileIDs==undefined) {
