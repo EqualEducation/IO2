9a8b2d079159b3016caa0d647d23b6426d630987
Carla Goldstein <carla.goldstein@gmail.com>
2016-05-10 21:00:27 +0200

    added warning landing page

---
+++ client/templates/shared/quotation.js
@@ -0,0 +1,25 @@
+Template.quotation.helpers({
+  quotationObject: function(activistIndex) {
+    console.log('ACTIVIST INDEX')
+    var quotationObject = new Object();
+    switch (activistIndex) {
+      case 1:
+        {
+          quotationObject.imageName = "martinlutherkingjr.jpeg"
+          quotationObject.quotation = "Freedom is never voluntarily given by the oppressor; it must be demanded by the oppressed."
+          quotationObject.name = "Martin Luther King, Jr."
+          break;
+        }
+      default:
+      {
+        quotationObject.imageName = "stevebiko.jpg"
+        quotationObject.quotation = "The most potent weapon in the hands of the oppressor is the mind of the oppressed."
+        quotationObject.name = "Steve Biko"
+        break;
+      }
+    }
+
+    console.log(quotationObject)
+    return quotationObject;
+  }
+})
