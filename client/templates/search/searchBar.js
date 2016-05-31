f8a687a98beedabe7d10da8cac1736f64d8259c7
Carla Goldstein <carla.goldstein@gmail.com>
2016-05-31 21:46:55 +0200

    enter button performs search

--- client/templates/search/searchBar.js
+++ client/templates/search/searchBar.js
@@ -12,5 +12,23 @@ Template.searchBar.events({
       }
 
     Session.set('searchText', text);
-  }
+  },
+  'keypress input': function(event, template) {
+    if (event.keyCode == 13) {
+        event.stopPropagation();
+        var text="";
+        if(template.find("#searchBox")==undefined)
+        {
+            text = "";
+        }
+        else
+        {
+          var searchBox = template.find("#searchBox");
+          text = $(searchBox).val().trim();
+        }
+
+      Session.set('searchText', text);
+        return false;
+    }
+}
 })
