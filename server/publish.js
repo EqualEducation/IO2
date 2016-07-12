1796f8ed9802835bf7bae4c571793d100ed8edd4
Carla Goldstein <carla.goldstein@gmail.com>
2016-07-12 10:16:02 +0200

    pagination working for activities only

--- server/publish.js
+++ server/publish.js
@@ -1,15 +1,18 @@
 searchItems = function(searchValue, pageSize, pageNumber, collection, fields) {
-  console.log('search items: ' + searchValue)
-  var paging = {};
-  if (pageSize != null && pageNumber != null && pageSize != undefined && pageSize != "" && pageNumber != undefined && pageNumber != "") {
-    paging = {skip: pageSize*(pageNumber-1), limit: parseInt(pageSize) }
+  console.log('search items: ' + searchValue);
+  var paging = {skip: 0, limit: 10};
+  if (pageSize !== null && pageNumber !== null && pageSize !== undefined && pageSize !== "" && pageNumber !== undefined && pageNumber !== "") {
+    paging = {skip: pageSize*(pageNumber-1), limit: parseInt(pageSize) };
   }
+  console.log('paging:');
   console.log(paging)
   // console.log(collection)
 
-  if (!searchValue ||  searchValue == "") {
+  if (!searchValue ||  searchValue === "") {
+    console.log('no search value');
     data = collection.find({}, paging);
   } else {
+    console.log('has search value');
     data = collection.find(
       { $text: {$search: searchValue} },
       {
@@ -56,6 +59,7 @@ Meteor.publish( 'files', function(){
 });
 
 Meteor.publish("resources-searchpage-count", function (searchValue) {
+  console.log('====COUNTING RESOURCES====');
    var fields = {score: { $meta: "textScore" }}
    var data = searchItems(searchValue, null, null, Resources, fields);
    if (data!=undefined && data.count() > 0 ) {
@@ -65,16 +69,32 @@ Meteor.publish("resources-searchpage-count", function (searchValue) {
 });
 
 
-Meteor.publish("activities-searchpage-count", function (searchValue) {
-  var fields = {score: { $meta: "textScore" }};
-  var data = searchItems(searchValue, null, null, Activities, fields);
-  if (data!=undefined && data.count() > 0 ) {
-    return data;
-  }
-  return this.ready();
-});
+// Meteor.publish("activities-searchpage-count", function (searchValue) {
+//   console.log('====COUNTING ACTIVITIES====');
+//   Counts.publish(this, 'activities-searchpage-count', Posts.find({ $text: {$search: searchValue} }));
+//
+//   var fields = {score: { $meta: "textScore" }};
+//   var itemCount = Activities.find(
+//     { $text: {$search: searchValue} },
+//     {
+//       // `fields` is where we can add MongoDB projections. Here we're causing
+//       // each document published to include a property named `score`, which
+//       // contains the document's search rank, a numerical value, with more
+//       // relevant documents having a higher score.
+//       // This indicates that we wish the publication to be sorted by the
+//       // `score` property specified in the projection fields above.
+//     }
+//   ).count();
+//
+//   if (itemCount!=undefined && itemCount > 0 ) {
+//     return itemCount;
+//   }
+//   return this.ready();
+// });
 
 Meteor.publish("curricula-searchpage-count", function (searchValue, pageSize, pageNumber) {
+  console.log('====COUNTING CURRICULA====');
+
   var fields = {score: { $meta: "textScore" }}
   var data = searchItems(searchValue, null, null, Curricula, fields);
   if (data!=undefined && data.count() > 0 ) {
@@ -84,6 +104,8 @@ Meteor.publish("curricula-searchpage-count", function (searchValue, pageSize, pa
 });
 
 Meteor.publish("resources-searchpage-data", function (searchValue, pageSize, pageNumber) {
+  console.log('====COUNTING RESOURCES DATA====');
+
    var fields = {'type':1,'itemType' : 1, 'details.title' : 1, 'details.description' : 1,  'details.keywords' : 1, score: { $meta: "textScore" }}
    var data = searchItems(searchValue, pageSize, pageNumber, Resources, fields);
    if (data!=undefined && data.count() > 0 ) {
@@ -93,6 +115,11 @@ Meteor.publish("resources-searchpage-data", function (searchValue, pageSize, pag
 });
 
 Meteor.publish("activities-searchpage-data", function (searchValue, pageSize, pageNumber) {
+  Counts.publish(this, 'activities-searchpage-count', Activities.find(), { noReady: true });
+
+  console.log('====COUNTING ACTIVITIES DATA====');
+
+  console.log("pageSize: " + pageSize +", pageNumber: " + pageNumber);
   var fields = {'itemType' : 1, 'details.title' : 1, 'details.description' : 1,  'details.keywords' : 1, score: { $meta: "textScore" }};
   var data = searchItems(searchValue, pageSize, pageNumber, Activities, fields);
   if (data!=undefined && data.count() > 0 ) {
@@ -102,6 +129,8 @@ Meteor.publish("activities-searchpage-data", function (searchValue, pageSize, pa
 });
 
 Meteor.publish("curricula-searchpage-data", function (searchValue, pageSize, pageNumber) {
+  console.log('====COUNTING CURRICULA DATA====');
+
   var fields = {'itemType' : 1, 'details.title' : 1, 'details.description' : 1,  'details.keywords' : 1, score: { $meta: "textScore" }};
   var data = searchItems(searchValue, pageSize, pageNumber, Curricula, fields);
   if (data!=undefined && data.count() > 0 ) {
