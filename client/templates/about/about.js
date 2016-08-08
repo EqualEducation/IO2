db929ea951cb5c6a487b3be7ce2feecb27baa5b9
Carla Goldstein <carla.goldstein@gmail.com>
2016-08-08 15:31:30 +0200

    added contact us

--- client/templates/about/about.js
+++ client/templates/about/about.js
@@ -1,9 +1,11 @@
 Template.about.onRendered(function() {
- Session.set('currentAboutPageId', 'about1');
-})
+  if (Session.get('currentAboutPageId') === undefined) {
+    Session.set('currentAboutPageId', 'about1');
+  }
+});
 
 Template.about.helpers({
   aboutPage : function() {
     return Session.get('currentAboutPageId');
   }
-})
+});
