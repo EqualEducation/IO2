288dfa398da8f5227d7365d88a9fb60b7c786528
Carla <carla.goldstein@ayudaheuristics.com>
2018-01-23 01:28:54 +0000

    Fixed minor bugs

--- client/templates/create/create resource/createResource.js
+++ client/templates/create/create resource/createResource.js
@@ -2,3 +2,10 @@ Template.registerHelper('totalNumberOfResourceType', function(resourceType) {
 	var numberOfItems = Resources.find({'type' : resourceType}).count();
 	return numberOfItems;
 });
+
+Template.registerHelper('resourceName', function(resourceName) {
+	if (resourceName == "Shortreading") {
+		return "Short Reading"
+	}
+	return resourceName;
+});
