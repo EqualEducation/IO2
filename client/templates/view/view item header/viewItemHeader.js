d9b884aade6e4135a9e843903e78d054b6f8cec5
Carla <carla.goldstein@ayudaheuristics.com>
2018-10-07 12:42:22 +0100

    Fixed error downloading pack

--- client/templates/view/view item header/viewItemHeader.js
+++ client/templates/view/view item header/viewItemHeader.js
@@ -1,8 +1,38 @@
+Template.viewItemHeader.onRendered(function() {
+  $('.download.button')
+  .popup({
+    popup : $('.custom.popup')
+  })
+;
+})
 
+Template.viewItemHeader.events({
+    'click #downloadPack' : function (event, template) {
+      event.preventDefault()
+      console.log("hello!")
+      console.log(this.item)
+      analytics.track("Download", {
+          "ID": this.item._id,
+          "Item Type": this.item.itemType,
+          "Name" : this.item.details.title
+      });
+      var htmlString = html2docx.generate('#detailsTable', template.css, template.stylesheets)
 
+        $('.ui.basic.downloadZip.modal')
+          .modal('setting', 'closable', false)
+          .modal('show')
+        ;
 
-// Book - Objects > Book
-// Video - Media > Film
-// Icebreaker - Message > Flag
-// Other - Message > Help
-// Short Reading - Computer and File System > File Text Outline
+       Meteor.call("createZipForDownload", this.item.itemType, this.item, htmlString, function(error, result){
+           if(error){
+             console.log(error)
+               alert(error);
+           }  else {
+            window.open(result,'_blank');
+          }
+          $('.ui.basic.downloadZip.modal')
+            .modal('hide')
+          ;
+      })
+    }
+  })
--- client/templates/view/viewItem.js
+++ client/templates/view/viewItem.js
@@ -1,12 +1,4 @@
 
-Template.viewItem.onRendered(function() {
-  $('.download.button')
-  .popup({
-    popup : $('.custom.popup')
-  })
-;
-})
-
 Template.registerHelper('listView', function(listItems) {
   var count=listItems.length;
   var returnList = "";
@@ -45,32 +37,7 @@ Template.registerHelper('convertDate', function(createdAtDate) {
   return month + ' ' + year;
 
 })
-// Template.viewItem.events({
-//   'click #downloadPDF2': function (event) {
-//     var doc = new jsPDF();
 
-// // We'll make our own renderer to skip this editor
-// var specialElementHandlers = {
-//   '#editor': function(element, renderer){
-//     return true;
-//   }
-// };
-
-// // All units are in the set measurement for the document
-// // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-// doc.fromHTML($('body').get(0), 15, 15, {
-//   'width': 170,
-//   'elementHandlers': specialElementHandlers
-// });
-// var pdf = new jsPDF('p','pt','a4');
-
-// pdf.addHTML(document.body,function() {
-//   var string = pdf.output('datauristring');
-//   $('.preview-pane').attr('src', string);
-// });
-// pdf.save('Test.pdf');
-//   }
-// });
 
 Template.viewItem.helpers({
   editURL: function(item) {
@@ -95,32 +62,6 @@ Template.viewItem.helpers({
 })
 
 Template.viewItem.events({
-    'click #downloadPack' : function (event, template) {
-      event.preventDefault()
-      analytics.track("Download", {
-          "ID": this._id,
-          "Item Type": this.itemType,
-          "Name" : this.details.title
-      });
-      var htmlString = html2docx.generate('#detailsTable', template.css, template.stylesheets)
-
-        $('.ui.basic.downloadZip.modal')
-          .modal('setting', 'closable', false)
-          .modal('show')
-        ;
-
-       Meteor.call("createZipForDownload", this.itemType, this, htmlString, function(error, result){
-           if(error){
-               alert(error);
-           }  else {
-            console.log(result);
-            window.open(result,'_blank');
-          }
-          $('.ui.basic.downloadZip.modal')
-            .modal('hide')
-          ;
-      })
-    },
     'click #deleteButton': function (event) {
       console.log("DELETE");
       itemID=(this._id);
